b235eb08e1786264383dd628c395cb9666ade4aa
Alessandro Farina <alefarina96@gmail.com>
2021-06-01 12:13:23 +0200

    Init folders for project

---
+++ src/screens/Profile.js
@@ -0,0 +1,20 @@
+import React , { Component } from 'react';
+//STYLE
+import './css/profile.css'
+
+class Profile extends Component {
+
+    constructor(props) {
+        super(props)
+    }
+
+    render() {
+        return(
+            <div className='profileContainer'>
+
+            </div>
+        );
+    }
+}
+
+export default Profile;
